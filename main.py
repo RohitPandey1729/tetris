7905747cc1c0c16b4ab96325822139ebc7d9dd94
Rohit Pandey <myboxrohit@gmail.com>
2024-08-28 19:48:42 -0700

    I added some more grids and drew a grid

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import pygame
 import random
 
+# creating the data structure for pieces
+# setting up global vars
+# functions
+# - create_grid
+# - draw_grid
+# - draw_window
+# - rotating shape in main
+# - setting up the main
+
 """
 10 x 20 square grid
 shapes: S, Z, I, O, J, L, T
@@ -13,7 +22,7 @@ pygame.font.init()
 s_width = 800
 s_height = 700
 play_width = 300  # meaning 300 // 10 = 30 width per block
-play_height = 600  # meaning 600 // 20 = 20 height per blo ck
+play_height = 600  # meaning 600 // 20 = 20 height per block
 block_size = 30
 
 top_left_x = (s_width - play_width) // 2
@@ -23,9 +32,9 @@ top_left_y = s_height - play_height
 # SHAPE FORMATS
 
 S = [['.....',
-      '.....',
-      '..00.',
-      '.00..',
+      '......',
+      '..00..',
+      '.00...',
       '.....'],
      ['.....',
       '..0..',
@@ -85,3 +94,107 @@ J = [['.....',
 L = [['.....',
       '...0.',
       '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '.0...',
+      '.....'],
+     ['.....',
+      '.00..',
+      '..0..',
+      '..0..',
+      '.....']]
+
+T = [['.....',
+      '..0..',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '..0..',
+      '.....']]
+
+shapes = [S, Z, I, O, J, L, T]
+shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
+# index 0 - 6 represent shape
+
+
+class Piece(object):
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.color = shape_colors[shapes.index(shape)]
+        self.rotation = 0  # number from 0-3
+	
+
+def create_grid(locked_positions={}):
+    grid = [[(0,0,0) for x in range(10)] for x in range(20)]
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (j,i) in locked_positions:            # Looking through this grid and finding the pos of the locked grid
+                c = locked_positions[(j,i)]
+                grid[i][j] = c
+    return grid
+          
+	
+
+def convert_shape_format(shape):
+	pass
+
+def valid_space(shape, grid):
+	pass
+
+def check_lost(positions):
+	pass
+
+def get_shape():
+	return random.choice(shapes)
+
+
+def draw_text_middle(text, size, color, surface):
+	pass
+   
+def draw_grid(surface, row, col):
+	surface.fill((0, 0, 0))
+	
+      pygame.font.init()
+      font = 
+
+def clear_rows(grid, locked):
+	pass
+
+def draw_next_shape(shape, surface):
+	pass
+      
+
+def draw_window(surface):
+	pass
+
+def main():
+	pass
+
+def main_menu():
+	pass
+
+main_menu()  # start game
